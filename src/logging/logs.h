205bce285404fa7b7fe4ebd8a09b75b3dd95e2af
sod-lol <ddssee5511@gmail.com>
2022-05-06 02:51:45 -0400

    Add color to logs

--- src/logging/logs.h
+++ src/logging/logs.h
@@ -21,7 +21,7 @@
 #if FATAL_ERROR_LOG_LEVEL < MINIMUM_LOG_LEVEL
 #define LOG_FATAL(error_code, fmt, ...) \
         do { \
-            fprintf(stderr, "[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+            fprintf(stderr, "\33[0;31m[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
             exit(error_code); \
         } while(0);
 #else
@@ -32,7 +32,7 @@
 #if ERROR_LOG_LEVEL < MINIMUM_LOG_LEVEL
 #define LOG_ERROR(fmt, ...) \
         do { \
-            fprintf(stderr, "[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+            fprintf(stderr, "\33[0;91m[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         } while(0);
 #else
 #define LOG_ERROR(fmt, ...) {}
@@ -42,7 +42,7 @@
 #if WARNING_LOG_LEVEL < MINIMUM_LOG_LEVEL
 #define LOG_WARNING(fmt, ...) \
         do { \
-            fprintf(stdout, "[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+            fprintf(stdout, "\033[0;93m[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         } while(0);
 #else
 #define LOG_WARNING(fmt, ...) {}
@@ -52,7 +52,7 @@
 #if INFO_LOG_LEVEL < MINIMUM_LOG_LEVEL
 #define LOG_INFO(fmt, ...) \
         do { \
-            fprintf(stdout, "[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+            fprintf(stdout, "\033[0;94m[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         } while(0);
 #else
 #define LOG_INFO(fmt, ...) {}
@@ -62,7 +62,7 @@
 #if VERBOSE_LOG_LEVEL < MINIMUM_LOG_LEVEL
 #define LOG_VERBOSE(fmt, ...) \
         do { \
-            fprintf(stdout, "[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
+            fprintf(stdout, "\33[0;96m[%s:%d:%s] "fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__); \
         } while(0);
 #else
 #define LOG_VERBOSE(fmt, ...) {}
