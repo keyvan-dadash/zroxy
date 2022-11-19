93c10c13b3f10c42667dd50736c8993707c9782c
sod-lol <ddssee5511@gmail.com>
2022-11-19 12:32:21 -0500

    Add doc for ssl connection and handle_client

--- src/connections/connections.h
+++ src/connections/connections.h
@@ -7,16 +7,44 @@
 #include "connections/conntypes/backend_types.h"
 #include "connections/conntypes/proxy_types.h"
 
-zxy_event_handler_t* make_proxy_event_handler(zxy_proxy_connection_t*, int, int8_t);
+/**
+ * Make a event handler for submit in epoll.
+ * \param proxy_obj The proxy connection settings that apply to the connection.
+ * \param sock The socket fd.
+ * \param sock_type The type of socket.
+ * \return zxy_event_handler_t Pointer to the event handler struct that contains necessery infos.
+ */
+zxy_event_handler_t* make_proxy_event_handler(zxy_proxy_connection_t* proxy_obj, int sock, int8_t sock_type);
 
-zxy_proxy_connection_t* zxy_make_proxy_connection(int32_t,
-            enum zxy_conn_type, 
-            int32_t,
-            enum zxy_conn_type);
+/**
+ * Make proxy connection based on connection type for each side.
+ * \param client_fd Obtained fd for the client.
+ * \param client_conn_type Type of connection for the client(e.g. ssl or plain).
+ * \param backend_fd Obtained fd for the backend.
+ * \param backend_conn_type Type of connection for the backend(e.g. ssl or plain).
+ * \return zxy_proxy_connection_t Pointer to the proxy connection struct which contains proxy settings for each side.
+ */
+zxy_proxy_connection_t* zxy_make_proxy_connection(
+            int32_t client_fd,
+            enum zxy_conn_type client_conn_type, 
+            int32_t backend_fd,
+            enum zxy_conn_type backend_conn_type);
 
-zxy_client_base_t* zxy_make_client_conn_with_type(int32_t, enum zxy_conn_type);
+/**
+ * Create client connection with specified connection type.
+ * \param client_fd Obtained fd for the client.
+ * \param client_conn_type Type of the connection for the client.
+ * \return zxy_client_base_t Pointer to the client base struct which holds client connection settings.
+ */
+zxy_client_base_t* zxy_make_client_conn_with_type(int32_t client_fd, enum zxy_conn_type client_conn_type);
 
-zxy_backend_base_t* zxy_make_backend_conn_with_type(int32_t, enum zxy_conn_type);
+/**
+ * Create backend connection with specified connection type.
+ * \param backend_fd Obtained fd for the backend.
+ * \param backend_conn_type Type of the connection for the backend.
+ * \return zxy_backend_base_t Pointer to the backend base struct which holds backend connection settings.
+ */
+zxy_backend_base_t* zxy_make_backend_conn_with_type(int32_t backend_fd, enum zxy_conn_type backend_conn_type);
 
 #endif /* CONNECTIONS_H */
 
--- src/connections/handle_client.h
+++ src/connections/handle_client.h
@@ -1,6 +1,12 @@
 #ifndef HANDLE_CLIENT_H
 #define HANDLE_CLIENT_H
 
+/**
+ * Handle each new connection.
+ * \param client_sock The socket fd of the new client.
+ * \param backend_host The network address of the backend which client should connect to. 
+ * \param backend_port The port of the backend which client should connect to.
+ */
 void zxy_handle_client_connection(int client_sock, char *backend_host, char *backend_port);
 
 #endif /* HANDLE_CLIENT_H */
