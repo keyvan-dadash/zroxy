d166f863866e5dd394e48c34faca1cd088440d12
sod-lol <ddssee5511@gmail.com>
2022-11-19 12:10:23 -0500

    Add doc for ssl connections

--- src/connections/ssl/backend_ssl_conn_req.h
+++ src/connections/ssl/backend_ssl_conn_req.h
@@ -5,12 +5,25 @@
 #include "connections/conntypes/proxy_types.h"
 #include "connections/conntypes/backend_types.h"
 
+/**
+ * Create basic ssl backend connection settings.
+ * \param params Parameters that should pass for creating backend base.
+ * \return zxy_backend_base_t Pointer to the created backend base.
+ */
 zxy_backend_base_t* zxy_make_ssl_backend_base_conn(void *params);
 
-/////////// ssl connection /////////////
-
+/**
+ * Creating backend ssl connection.
+ * \param ctx The SSL_CTX that use in ssl connection procedure.
+ * \param sock_fd Socket fd that use in ssl connection.
+ * \return zxy_backend_ssl_conn_t Pointer to the created backend ssl connection.
+ */
 zxy_backend_ssl_conn_t* zxy_make_backend_ssl_conn(SSL_CTX *ctx, int sock_fd);
 
+/**
+ * Set callbacks on backend base.
+ * \param backend_base Pointer to the backend base for setting callbacks.
+ */
 void zxy_set_up_backend_ssl_base_callbacks(zxy_backend_base_t* backend_base);
 
 #endif /* BACKEND_SSL_CONN_REQ_H */
--- src/connections/ssl/client_ssl_conn_req.h
+++ src/connections/ssl/client_ssl_conn_req.h
@@ -5,12 +5,25 @@
 #include "connections/conntypes/proxy_types.h"
 #include "connections/conntypes/client_types.h"
 
+/**
+ * Create basic ssl client connection settings.
+ * \param params Parameters that should pass for creating client base.
+ * \return zxy_client_base_t Pointer to the created client base.
+ */
 zxy_client_base_t* zxy_make_ssl_client_base_conn(void *params);
 
-/////////// ssl connection /////////////
-
+/**
+ * Creating client ssl connection.
+ * \param ctx The SSL_CTX that use in ssl connection procedure.
+ * \param sock_fd Socket fd that use in ssl connection.
+ * \return zxy_client_ssl_conn_t Pointer to the created client ssl connection.
+ */
 zxy_client_ssl_conn_t* zxy_make_client_ssl_conn(SSL_CTX *ctx, int sock_fd);
 
+/**
+ * Set callbacks on client base.
+ * \param client_base Pointer to the client base for setting callbacks.
+ */
 void zxy_set_up_client_ssl_base_callbacks(zxy_client_base_t* client_base);
 
 #endif /* CLIENT_SSL_CONN_REQ_H */
