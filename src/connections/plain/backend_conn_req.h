d47932c97d76753714108eaeda7333ac4575945a
sod-lol <ddssee5511@gmail.com>
2022-11-19 12:00:39 -0500

    Add doc for plain connections

--- src/connections/plain/backend_conn_req.h
+++ src/connections/plain/backend_conn_req.h
@@ -4,11 +4,24 @@
 #include "connections/conntypes/proxy_types.h"
 #include "connections/conntypes/backend_types.h"
 
+/**
+ * Create basic backend connection settings.
+ * \param params Parameters that should pass for creating backend base.
+ * \return zxy_backend_base_t Pointer to the created backend base.
+ */
 zxy_backend_base_t* zxy_make_plain_backend_base_conn(void *params);
 
-//////// plain connections
+/**
+ * Creating backend plain connection.
+ * \param sock_fd Socket fd that use in plain connection.
+ * \return zxy_backend_conn_t Pointer to the created backend plain connection.
+ */
 zxy_backend_conn_t* zxy_make_backend_plain_conn(int sock_fd);
 
+/**
+ * Set callbacks on backend base.
+ * \param backend_base Pointer to the backend base for setting callbacks.
+ */
 void zxy_set_up_backend_plain_base_callbacks(zxy_backend_base_t* backend_base);
 
 #endif /* BACKEND_CONN_REQ_H */
--- src/connections/plain/client_conn_req.h
+++ src/connections/plain/client_conn_req.h
@@ -5,12 +5,24 @@
 #include "connections/conntypes/proxy_types.h"
 #include "connections/conntypes/client_types.h"
 
+/**
+ * Create basic client connection settings.
+ * \param params Parameters that should pass for creating client base.
+ * \return zxy_client_base_t Pointer to the created client base.
+ */
 zxy_client_base_t* zxy_make_plain_client_base_conn(void *params);
 
-/////////// pain connection /////////////
-
+/**
+ * Creating client plain connection.
+ * \param sock_fd Socket fd that use in plain connection.
+ * \return zxy_client_conn_t Pointer to the created client plain connection.
+ */
 zxy_client_conn_t* zxy_make_client_plain_conn(int sock_fd);
 
+/**
+ * Set callbacks on client base.
+ * \param client_base Pointer to the client base for setting callbacks.
+ */
 void zxy_set_up_client_plain_base_callbacks(zxy_client_base_t* client_base);
 
 #endif /* CLIENT_CONN_REQ_H */
